f99feaead14e3f874219ebe3f2744aa1144b00b3
Max <89098307231max@mail.ru>
2025-03-03 13:30:17 +0300

    Add solution

---
+++ Lesson1/task3/NetoLess1Task3/NetoLess1Task3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+struct Address {
+    std::string city;
+    std::string street;
+    int houseNumber;
+    int apartmentNumber;
+    int index;
+};
+
+void printAddress(Address& address);
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    Address address1 = { "Москва", "Арбат", 12, 8, 123456 };
+    Address address2 = { "Ижевск", "Пушкина", 59, 143, 953769 };
+    printAddress(address1);
+    std::cout << '\n';
+    printAddress(address2);
+    return 0;
+}
+
+void printAddress(Address& address) {
+    std::cout << "Город: " << address.city << std::endl;
+    std::cout << "Улица: " << address.street << std::endl;
+    std::cout << "Номер дома: " << address.houseNumber << std::endl;
+    std::cout << "Номер квартиры: " << address.apartmentNumber << std::endl;
+    std::cout << "Индекс: " << address.index << std::endl;
+}
