f3d6cc635c582833846fd3479c3a357eff298e5e
MaximSH95 <89098307231max@mail.ru>
2025-03-06 18:38:15 +0300

    Add solution for lesson 2

---
+++ Lesson2/task1/NetoLess2Task1/NetoLess2Task1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+
+class Calculator {
+private:
+    double num1{};
+    double num2{};
+
+public:
+    Calculator(double set_num1 = 0, double set_num2 = 0) : num1{ set_num1 }, num2{ set_num2 } {}
+    double add() {
+        return num1 + num2;
+    }
+    double multiply() {
+        return num1 * num2;
+    }
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
+    }
+
+    bool set_num1() {
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+        if (num1 != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    bool set_num2() {
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+        if (num2 != 0) {
+            return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    Calculator calc{};
+    while(true){
+        while (!calc.set_num1()) {
+            std::cout << "Неверный ввод!" << std::endl;
+        }
+
+        while (!calc.set_num2()) {
+            std::cout << "Неверный ввод!" << std::endl;
+        }
+
+        std::cout << "num1 + num2 = " << calc.add() << std::endl;
+        std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+    }
+    return 0;
+}
---
+++ Lesson2/task2/NetoLess2Task2/NetoLess2Task2.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class Counter {
+    private:
+        int num{};
+
+    public:
+        Counter(int set_num = 1 ) : num{ set_num } {}
+        void setNum(int set_num) {
+            num = set_num;
+        }
+
+        int add() {
+            return ++num;
+        }
+        int subtract() {
+            return --num;
+        }
+        int sol()
+        {
+            return num;
+        }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Counter counter;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::string start;
+    std::getline(std::cin >> std::ws, start);
+    char ch ;
+
+    if (start == "да") {
+        std::cout << "Введите начальное значение счетчика: ";
+        int init_num;
+        std::cin >> init_num;
+        counter.setNum(init_num);
+        while (true) {
+            std::cout << "Введите команду ('+' , '-', '=', или 'x'): ";
+            std::cin >> ch;
+            if (ch == '+') {
+                counter.add();
+            }
+            if (ch == '-') {
+                counter.subtract();
+            }
+            if (ch == '=') {
+                std::cout << counter.sol() << std::endl;
+            }
+            if (ch == 'x') {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+        }
+    }
+    if (start == "нет") {
+        std::cout << "Счетчик по умолчанию 1" << std::endl;
+        while (true) {
+            std::cout << "Введите команду ('+' , '-', '=', или 'x'): ";
+            std::cin >> ch;
+            if (ch == '+') {
+                counter.add();
+            }
+            if (ch == '-') {
+                counter.subtract();
+            }
+            if (ch == '=') {
+                std::cout << counter.sol() << std::endl;
+            }
+            if (ch == 'x') {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
