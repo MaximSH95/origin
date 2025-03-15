c25918c3277ea706e5d1956f1b6c95f5f4e5665e
MaximSH95 <89098307231max@mail.ru>
2025-03-15 15:59:15 +0300

    fixed less4 task1

--- Lesson4/NetoLess4Task1/ConsoleApplication1.cpp
+++ Lesson4/NetoLess4Task1/ConsoleApplication1.cpp
@@ -1,41 +1,39 @@
 ﻿#include <iostream>
+#include <string>
 
 class Figure
 {
 protected:
     int sides_count;
-public:Figure(int sides_count = 0) {
+    std::string name;
+public:
+    Figure(std::string name, int sides_count = 0) {
+    this->name = name;
     this->sides_count = sides_count;
 }
 void get_sides_count() {
-    std::cout << "Фигура: " << sides_count << std::endl;
+    std::cout << name << ": " << sides_count << std::endl;
 }
 };
 
 class Triangle : public Figure {
 public: 
-    Triangle(int sides_count) :Figure(sides_count) {}
-    void get_sides_count(){
-         std::cout << "Треугольник: " << sides_count << std::endl;
-    }
+    Triangle(std::string name, int sides_count) :Figure(name, sides_count) {}
 };
 class Quadrangle : public Figure {
 public:
-    Quadrangle (int sides_count) :Figure(sides_count) {}
-    void get_sides_count() {
-        std::cout << "Четырёхугольник: " << sides_count << std::endl;
-    }
+    Quadrangle (std::string name, int sides_count) :Figure(name, sides_count) {}
 };
 
 int main()
 {
     setlocale(LC_ALL, "RU");
     std::cout << "Количество сторон: " << std::endl;
-    Figure figure(0);
+    Figure figure("Фигура", 0);
     figure.get_sides_count();
-    Triangle triangle(3);
+    Triangle triangle("Треугольник", 3);
     triangle.get_sides_count();
-    Quadrangle quadrangle(4);
+    Quadrangle quadrangle("Четырёхугольник", 4);
     quadrangle.get_sides_count();
 
     return 0;
